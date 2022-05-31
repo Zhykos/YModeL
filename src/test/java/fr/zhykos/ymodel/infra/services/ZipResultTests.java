6111e7d90f810d3657f9f0d98e8d34643c980b78
Thomas Cicognani <tcicognani@norsys.fr>
2022-05-31 22:57:47 +0200

    tests(TDD): create a test which verify the final zip files which will be send to the client

--- src/test/java/fr/zhykos/ymodel/infra/services/ZipResultTests.java
+++ src/test/java/fr/zhykos/ymodel/infra/services/ZipResultTests.java
@@ -1,31 +1,47 @@
 package fr.zhykos.ymodel.infra.services;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 class ZipResultTests {
 
     @Test
     @DisplayName("Zip some strings into a single file")
-    void zip(@TempDir final File tempDir) throws IOException {
+    void zip() throws IOException {
+        final FileSystem fileSystem = Jimfs.newFileSystem(Configuration.unix());
+
         final Map<String, String> filesContent = Map.of("file01.txt", "foo", "file02.txt", "hello there");
 
-        final File zipFile = File.createTempFile("result", ".zip", tempDir);
-        try (FileOutputStream fileOutputStream = new FileOutputStream(zipFile)) {
-            new ZipResult().zip(filesContent, fileOutputStream);
+        final Path junitPath = fileSystem.getPath("junit");
+        Files.createDirectory(junitPath);
+        final Path zipPath = Files.createTempFile(junitPath, "result", ".zip");
+        try (OutputStream outputStream = Files.newOutputStream(zipPath)) {
+            new ZipResult().zip(filesContent, outputStream);
         }
 
-        Assertions.assertTrue(Files.mismatch(zipFile.toPath(), Path.of("src/test/resources/expected01.zip")) < 0,
-                () -> "Files don't match! Generated zip is: '%s'".formatted(zipFile.getAbsolutePath()));
+        Assertions.assertTrue(Files.mismatch(zipPath, Path.of("src/test/resources/expected01.zip")) < 0,
+                () -> {
+                    final var debugPath = Path.of("src/test/resources/debug/expected01.zip");
+                    var assertMessage = "Files don't match! Generated zip is: '%s'"
+                            .formatted(debugPath.toAbsolutePath());
+                    try {
+                        Files.copy(zipPath, debugPath);
+                    } catch (IOException e) {
+                        assertMessage += "\nCannot export generated zip because of: " + e.getMessage();
+                    }
+                    return assertMessage;
+                });
     }
 
 }
