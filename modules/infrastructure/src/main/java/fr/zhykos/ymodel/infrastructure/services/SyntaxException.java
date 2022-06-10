b7a7f54da2076da535e59427ae3f660aff54a3b6
Thomas Cicognani <tcicognani@norsys.fr>
2022-06-10 12:51:13 +0200

    doc(checkstyle): add javadoc

--- modules/infrastructure/src/main/java/fr/zhykos/ymodel/infrastructure/services/SyntaxException.java
+++ modules/infrastructure/src/main/java/fr/zhykos/ymodel/infrastructure/services/SyntaxException.java
@@ -1,11 +1,24 @@
 package fr.zhykos.ymodel.infrastructure.services;
 
+/**
+ * Syntax exception thrown when a syntax error is detected in a YML file
+ */
 public final class SyntaxException extends Exception {
 
+    /**
+     * Constructor
+     *
+     * @param cause Cause of the exception
+     */
     public SyntaxException(final Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructor
+     *
+     * @param message Message of the exception
+     */
     public SyntaxException(final String message) {
         super(message);
     }
