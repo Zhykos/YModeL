2f7b1d71a9daad7188c673824f851c5ef2f209ce
Thomas Cicognani <tcicognani@norsys.fr>
2022-06-10 12:29:43 +0200

    refactor: all TransformationServiceTestst in a single file

--- modules/infrastructure/src/test/java/fr/zhykos/ymodel/infrastructure/services/TransformationServiceTests.java
+++ modules/infrastructure/src/test/java/fr/zhykos/ymodel/infrastructure/services/TransformationServiceTests.java
@@ -1,5 +1,7 @@
 package fr.zhykos.ymodel.infrastructure.services;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
@@ -16,7 +18,7 @@ import fr.zhykos.ymodel.infrastructure.models.yml.YmlMetaModel;
 import fr.zhykos.ymodel.infrastructure.models.yml.YmlMethod;
 import fr.zhykos.ymodel.infrastructure.models.yml.YmlMethodParameter;
 
-class TransformationService01Tests {
+class TransformationServiceTests {
 
     @Test
     @DisplayName("Transform a YML metamodel into an EMF metamodel")
@@ -111,6 +113,59 @@ class TransformationService01Tests {
         method01.getParameters().add(param02);
         method03.getParameters().add(param03);
         return metaModel;
+    }
+
+    @Test
+    @DisplayName("Transform a YML metamodel into an EMF metamodel and check unreferenced types")
+    void transformUnreferencedTypes() throws IOException {
+        final File yamlFile = new File("src/test/resources/metamodel03.yml");
+        final Returns<YmlMetaModel, IOException> parseReturns = new ParsingService().parse(yamlFile);
+        final YmlMetaModel ymlMetaModel = parseReturns.then();
+
+        final Returns<List<EClass>, SemanticListException> eClasses = new TransformationService()
+                .transform(ymlMetaModel);
+        Assertions.assertThrows(SemanticListException.class, () -> eClasses.catchh());
+
+        try {
+            eClasses.catchh();
+        } catch (SemanticListException e) {
+            Assertions.assertEquals(
+                    "Unknown class reference for inheritance: $Class01\nUnknown class reference '$Class01' in element 'field01'.\nUnknown class reference '$Class01' in element 'method01'.\nUnknown class reference '$Class01' in element 'param01'.",
+                    e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Transform a YML metamodel into an EMF metamodel and check referenced types")
+    void transformReferencedTypes() throws SemanticListException, IOException {
+        final File yamlFile = new File("src/test/resources/metamodel02.yml");
+        final Returns<YmlMetaModel, IOException> parseReturns = new ParsingService().parse(yamlFile);
+        final YmlMetaModel ymlMetaModel = parseReturns.then();
+
+        final Returns<List<EClass>, SemanticListException> eClassesReturns = new TransformationService()
+                .transform(ymlMetaModel);
+        final List<EClass> eClasses = eClassesReturns.then();
+        Assertions.assertEquals(2, eClasses.size());
+
+        final EClass class01 = eClasses.get(0);
+        Assertions.assertEquals(0, class01.getESuperTypes().size());
+        Assertions.assertEquals("Class01", class01.getName());
+        Assertions.assertEquals(0, class01.getEOperations().size());
+        Assertions.assertEquals(0, class01.getEAttributes().size());
+
+        final EClass class02 = eClasses.get(1);
+        Assertions.assertEquals(0, class02.getESuperTypes().size());
+        Assertions.assertEquals("Class02", class02.getName());
+        Assertions.assertEquals(1, class02.getEOperations().size());
+
+        final EOperation method01 = class02.getEOperations().get(0);
+        Assertions.assertEquals("method01", method01.getName());
+        Assertions.assertEquals(class01, method01.getEType());
+        Assertions.assertEquals(1, method01.getEParameters().size());
+
+        final EParameter param03 = method01.getEParameters().get(0);
+        Assertions.assertEquals("param01", param03.getName());
+        Assertions.assertEquals(class01, param03.getEType());
     }
 
 }
